9a3b5ef0ab87b5a2235bc3a89955d9d81ccfd964
shaaravbarfe-boop <shaaravbarfe@gmail.com>
2026-01-07 15:30:58 +0530

    Create __init__.py

---
+++ src/pluginname/src/pluginname/src/pluginname/__init__.py
@@ -0,0 +1,11 @@
+from endstone.plugin import Plugin
+
+class MyPlugin(Plugin):
+    def on_load(self) -> None:
+        self.logger.info("on_load is called!")
+
+    def on_enable(self) -> None:
+        self.logger.info("on_enable is called!")
+
+    def on_disable(self) -> None:
+        self.logger.info("on_disable is called!")
