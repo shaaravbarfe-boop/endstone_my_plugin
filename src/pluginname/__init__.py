707b5157bec097f899aeb906492b77976d543dd1
shaaravbarfe-boop <shaaravbarfe@gmail.com>
2026-01-07 17:23:11 +0530

    Delete src/pluginname directory

--- src/pluginname/__init__.py
+++
@@ -1,8 +0,0 @@
-[build-system]
-requires = ["hatchling"]
-build-backend = "hatchling.build"
-
-[project]
-name = "endstone-my-plugin"
-version = "0.1.0"
-description = "My first Python plugin for Endstone servers!"
--- src/pluginname/src/pluginname/__init__.py
+++
@@ -1,7 +0,0 @@
-from endstone.plugin import Plugin
-
-class MyPlugin(Plugin):
-    pass
-  from endstone_my_plugin.my_plugin import MyPlugin
-
-__all__ = ["MyPlugin"]
